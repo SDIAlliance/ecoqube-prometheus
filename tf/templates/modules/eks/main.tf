6afdfe1b90bfa2c5dfde07cdc9bd34b2050ca95c
Maarten Kamoen <maarten@9apps.net>
2021-10-05 14:37:42 +0200

    Added external DNS

--- tf/templates/modules/eks/main.tf
+++ tf/templates/modules/eks/main.tf
@@ -262,6 +262,55 @@ resource "aws_iam_role_policy_attachment" "aws_lb_controller_asg_policy_attachme
   policy_arn = aws_iam_policy.aws_lb_controller_asg_policy.arn
 }
 
+# External DNS
+##############
+data "aws_iam_policy_document" "external_dns_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:external-dns"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "external_dns_role" {
+  assume_role_policy = data.aws_iam_policy_document.external_dns_policy.json
+  name               = "${var.project}-${var.env}-eks-service-account-role-external-dns"
+}
+
+data "aws_iam_policy_document" "external_dns_policy_document" {
+  statement {
+    actions   = ["route53:ChangeResourceRecordSets"]
+    resources = ["arn:aws:route53:::hostedzone/*"]
+    effect    = "Allow"
+  }
+  statement {
+    actions   = ["route53:ListHostedZones", "route53:ListResourceRecordSets"]
+    resources = ["*"]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "external_dns_policy" {
+  name   = "${var.project}-${var.env}-eks-service-account-role-external_dns-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.external_dns_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns_policy_attachment" {
+  role       = aws_iam_role.external_dns_role.name
+  policy_arn = aws_iam_policy.external_dns_asg_policy.arn
+}
+
 # EFS Role
 ##########
 data "aws_iam_policy_document" "efs_csi_controller_policy" {
