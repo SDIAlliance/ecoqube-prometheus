fc555f3ba8445377e2db2f34e3e7d8fdf195f3a2
maxschulze <max@maxschulze.com>
2024-07-27 14:54:43 +0200

    Update configure_cluster.sh
    fix autoscaler version

--- scripts/configure_cluster.sh
+++ scripts/configure_cluster.sh
@@ -9,6 +9,9 @@ then
 elif [[ $KUBERNETES_VERSION == 1.21 ]]
 then
   export CLUSTER_AUTOSCALER_VERSION=v1.21.0
+elif [[ $KUBERNETES_VERSION == 1.28 ]]
+then
+  export CLUSTER_AUTOSCALER_VERSION=v1.28.0
 else
   echo "Error: no versions defined for Kubernetes $KUBERNETES_VERSION"
   exit 1
